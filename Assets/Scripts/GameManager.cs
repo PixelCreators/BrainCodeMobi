45c4959a4bbc9ca37b9c4edbb471a81b618a5e71
mathewp <mateusz.pusty@gmail.com>
2015-03-13 21:28:16 +0100

    Dodano scene i managery

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+    static public GameManager instance;
+
+    void Start()
+    {
+        if (instance == null)
+            instance = gameObject;
+        else
+            Destroy(gameObject);
+    }
+
+ 
+    void Update()
+    {
+    }
+}
