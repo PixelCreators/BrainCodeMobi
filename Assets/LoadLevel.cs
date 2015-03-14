349f9b0b011b471024b8c8912950ed5a2c4a0073
gymon <szymon.galdyn@gmail.com>
2015-03-14 12:01:40 +0100

    Menu up

---
+++ Assets/LoadLevel.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadLevel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Application.LoadLevel("Game");
+	
+	}
+}
