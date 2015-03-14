67259503b01ebb06d73f9b5779610d5694f3a2b7
mathewp <mateusz.pusty@gmail.com>
2015-03-14 11:44:17 +0100

    Dodano wybuchy ptaszkow v2

---
+++ Assets/Scripts/DestroyParticleAfterScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof (ParticleSystem))]
+
+public class DestroyParticleAfterScript : MonoBehaviour
+{
+    private ParticleSystem particleSystemComponent;
+
+    //Jeśli różne od zera to cząsteczka jest niszczona po danym czasie.
+    public float DestroyParticleAfterTime = 0f;
+
+    private void Awake()
+    {
+        particleSystemComponent = GetComponent<ParticleSystem>();
+    }
+
+    private void Update()
+    {
+        if (DestroyParticleAfterTime > 0)
+            Destroy(this.gameObject, DestroyParticleAfterTime);
+        else
+            if (!particleSystemComponent.isPlaying)
+                Destroy(this.gameObject);
+
+    }
+}
