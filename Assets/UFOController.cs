7b5620e332dc3b5958f2802ca231c2760fede45d
mathewp <mateusz.pusty@gmail.com>
2015-03-14 16:19:33 +0100

    Działa wszystko oprócz highscores

--- Assets/UFOController.cs
+++ Assets/UFOController.cs
@@ -4,18 +4,27 @@ using System.Collections;
 public class UFOController : MonoBehaviour
 {
     PlayerController playerController;
-
+    public AudioClip ufoSound;
+    private Transform killZone;
     public float time = 5.0f;
 
     void Start()
     {
+        killZone = GameObject.Find("BackgroundDespawnPoint").transform;
         playerController = FindObjectOfType<PlayerController>();
+    }
+
+    void Update()
+    {
+        if (killZone.position.y > transform.position.y)
+            Destroy(gameObject);
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if(other.tag == "Player")
         {
+            AudioSource.PlayClipAtPoint(ufoSound, Vector3.zero);
             StartCoroutine(playerController.InverseAxis(time));
         }
     }
